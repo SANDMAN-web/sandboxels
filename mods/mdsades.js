0feb54e833aa839125be637cf3ea0c78f0a7de6b
SANDMAN-web <masonz28@student.newton.k12.in.us>
2025-01-31 11:27:57 -0600

    Update mdsades.js

--- mods/mdsades.js
+++ mods/mdsades.js
@@ -1,1 +1,27 @@
-mdsades
+elements.reality = {
+    color: ["#FF2121FF", "#FF5757FF],
+    behavior: behaviors.POWDER,
+    category: "infinity",
+        state: "solid",
+        reactions: {
+            "dirt": { elem1: "reality", elem2: "sand" },
+            "grenade": { elem1: "reality", elem2: "bubble" },
+            "electric": { elem1: "reality", elem2: "electric" },
+            "dust": { elem1: "reality", elem2: "gold" },
+            "sun": { elem1: "reality", elem2: "supernova" },
+            "dna": { elem1: "reality", elem2: "ant" },
+            "ice": { elem1: "reality", elem2: "fire" },
+            "magma": { elem1: "reality", elem2: "water" }
+            "rock": { elem1: "reality_stone" }
+        },
+}
+elements.reality_stone = {
+    color: ["#CF0000FF", "#DE0000FF],
+    behavior: behaviors.CLONER,
+    ignore: ["ecloner","slow_cloner","clone_powder","floating_cloner","wall","ewall"],
+        category:"infinity",
+        insulate:true,
+        hardness: 1,
+        darkText: true,,
+       },
+}
